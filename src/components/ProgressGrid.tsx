c5732ec71dffc571249bedf0aab06d6a9c501a79
David Ventura <david@davidv.dev>
2025-01-05 20:22:33 +0100

    align exam icon with CSS, remove hacks

--- src/components/ProgressGrid.tsx
+++ src/components/ProgressGrid.tsx
@@ -69,47 +69,48 @@ export const ProgressGrid = ({
       <div className='gap-3 grid grid-cols-[36px_repeat(3,_minmax(0,_1fr))]'>
         {terms2.map((t) => (
           <>
-            {t.days.map((day, idx) => (
-              <>
-                {idx == 0 ? (
-                  <div
-                    className={cn('rounded-full place-self-center p-2 border', {
-                      'bg-green-100 border-green-600 text-green-600':
-                        t.exam && t.exam.level != 'fail',
-                      'bg-red-100 border-red-600 text-red-600':
-                        t.exam && t.exam.level == 'fail',
-                      'bg-gray-100 border-gray-100 text-gray-600':
-                        t.exam == null
-                    })}
-                  >
-                    <Dumbbell strokeWidth={1.5} className='w-4 h-4'></Dumbbell>
-                  </div>
-                ) : idx % 3 == 0 && (
-                  <div></div>
-                )}
-                <div
-                  className={cn('p-2 rounded-md outline-none relative', {
-                    'bg-gray-100': day.status === undefined,
-                    'bg-green-100': day.status === 'done',
-                    'bg-red-100': day.status === 'failed',
-                    'outline outline-blue-600 outline-offset-[-1px] ':
-                      day.status === undefined && day.day === currentDay
-                  })}
-                >
-                  <h4 className='text-xs text-gray-700'>Day {day.day + 1}</h4>
-                  <span className={cn({ 'text-gray-300': t.exam === null })}>
-                    {t.exam ? day.pushupCount : 'N/A'}
-                  </span>
-                  {'status' in day &&
-                    (day.status === 'done' ? (
-                      <CheckCircle2 className='absolute bottom-1 right-2 w-4 text-green-500'></CheckCircle2>
-                    ) : day.status === 'failed' ? (
-                      <XCircle className='absolute bottom-1 right-2 w-4 text-red-500'></XCircle>
-                    ) : (
-                      <></>
-                    ))}
-                </div>
-              </>
+            <div
+              className={cn('rounded-full place-self-start p-2 border', {
+                'bg-green-100 border-green-600 text-green-600':
+                  t.exam && t.exam.level != 'fail',
+                'bg-red-100 border-red-600 text-red-600':
+                  t.exam && t.exam.level == 'fail',
+                'bg-gray-100 border-gray-100 text-gray-600': t.exam == null,
+                'row-span-2': t.days.length == 6,
+                'row-span-3': t.days.length == 9,
+                // The icon spans two rows verically, and the rows
+                // have gap of "gap-3" between them, which is 0.75rem = 12px
+                // so we need to substract the entire gap from the Y coordinate
+                'translate-y-[calc(50%_-_6px)]': true
+              })}
+            >
+              <Dumbbell strokeWidth={1.5} className='w-4 h-4'></Dumbbell>
+            </div>
+            {t.days.map((day) => (
+              <div
+                className={cn('p-2 rounded-md outline-none relative', {
+                  'bg-gray-100': day.status === undefined,
+                  'bg-green-100': day.status === 'done',
+                  'bg-red-100': day.status === 'failed',
+                  'outline-1 outline-red-600 outline-offset-[-1px] ':
+                    day.status !== undefined && day.day === currentDay,
+                  'outline-1 outline-indigo-600 outline-offset-[-1px] ':
+                    day.status === undefined && day.day === currentDay
+                })}
+              >
+                <h4 className='text-xs text-gray-700'>Day {day.day + 1}</h4>
+                <span className={cn({ 'text-gray-300': t.exam === null })}>
+                  {t.exam ? day.pushupCount : 'N/A'}
+                </span>
+                {'status' in day &&
+                  (day.status === 'done' ? (
+                    <CheckCircle2 className='absolute bottom-1 right-2 w-4 text-green-500'></CheckCircle2>
+                  ) : day.status === 'failed' ? (
+                    <XCircle className='absolute bottom-1 right-2 w-4 text-red-500'></XCircle>
+                  ) : (
+                    <></>
+                  ))}
+              </div>
             ))}
           </>
         ))}
